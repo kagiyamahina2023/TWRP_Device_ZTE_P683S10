99f99e01c8d207a2c99254c8069d84e39ea9e451
madegh <167677067+madegh@users.noreply.github.com>
2024-04-25 12:27:01 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo twrp_P683S10-user
-add_lunch_combo twrp_P683S10-userdebug
-add_lunch_combo twrp_P683S10-eng
+add_lunch_combo omni_P683S10-user
+add_lunch_combo omni_P683S10-userdebug
+add_lunch_combo omni_P683S10-eng
